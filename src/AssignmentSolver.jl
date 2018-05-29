1ea2124bf3ae721b3a71e85af123520b11c503c4
Brendan McVeigh <mcveigh.brendan@gmail.com>
2018-05-29 09:41:44 -0400

    add auction algorithms for sparse problems, do not currently pass all tests

--- src/AssignmentSolver.jl
+++ src/AssignmentSolver.jl
@@ -6,7 +6,22 @@ export lsap_solver, lsap_solver!, adjusted_cost
 #export step1!, step2!, step3!, step4!, step5!, step6!
 export lsap_solver_tracking, lsap_solver_tracking!
 #export step3_tracking!, step4_tracking!, step5_tracking!, step6_tracking!
-export forward_backward, symmetric_forward_backward, asymmetric_forward_backward
+export maxtwoRow,
+    maxtwoCol,
+    maxtwoCol_shadow,
+    forward_iteration,
+    forward_iteration_shadow,
+    backward_iteration,
+    backward_iteration_shadow,
+    symmetric_forward_backward,
+    asymmetric_forward_backward,
+    asymmetric_forward_backward_shadow,
+    symmetric_scaling_forward_backward,
+    asymmetric_scaling_forward_backward,
+    asymmetric_scaling_forward_backward_shadow,
+    scaling_forward_backward,
+    scaling_forward_backward_shadow,
+    add_dummy_entries
 #export maxtwoRow, maxtwoCol, forward_iteration, backward_iteration
 include("solver.jl")
 include("tracking.jl")
